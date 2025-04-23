d31b8b0572add66c16ca857e235bac766a4f5e38
Michael Buckley <michaelcbuckley@proton.me>
2025-04-23 16:11:35 -0400

    Commented out the substitutors for now

--- flake.nix
+++ flake.nix
@@ -1,16 +1,17 @@
 {
   description = "Microvm for testing Hashicorp Vault";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://microvm.cachix.org"
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
+  # Optional Subsitution (these are enable on my system-wide config)
+  # nixConfig = {
+  #   extra-substituters = [
+  #     "https://microvm.cachix.org"
+  #     "https://nix-community.cachix.org"
+  #   ];
+  #   extra-trusted-public-keys = [
+  #     "microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys="
+  #     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  #   ];
+  # };
 
   inputs = {
     nixpkgs.follows = "microvm/nixpkgs";
