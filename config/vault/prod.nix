5854f0a5dcb9368ed8816f811b27f1b730afc9b2
Michael Buckley <michaelcbuckley@proton.me>
2025-04-24 14:44:10 -0400

    Made tweaks to get the basic prod edition functional OOTB

--- config/vault/prod.nix
+++ config/vault/prod.nix
@@ -3,20 +3,37 @@ _: {
   services.vault = {
     enable = true;
     address = "0.0.0.0:8200";
-    storageBackend = "file";
-    listenerExtraConfig = ''tls_disable = 1'';
+    listenerExtraConfig = ''
+      tls_disable = 1
+    '';
+
+    storageBackend = "raft";
+    storageConfig = ''
+      path = "/vault"
+      node_id = "node1"
+    '';
+    
     extraConfig = ''
-      disable_mloc = true
       ui = true
+
+      api_addr = "http://localhost:8200"
+
+      # Cluster address must be set when using raft
+      cluster_addr = "http://127.0.0.1:8201"
     '';
   };
 
   # Give the production unit a persistent file store
   microvm.volumes = [
     {
-      mountPoint = "/var";
+      mountPoint = "/vault";
       image = "vault.img";
       size = 512;
     }
+  ];
+
+  # Change the persmissions for the directory
+  systemd.tmpfiles.rules = [
+    "d /vault 0750 vault vault -"
   ];
 }
