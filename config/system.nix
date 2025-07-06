4a658c67ff62988dcf0803575579cc3153bc1072
Michael Buckley <michaelcbuckley@proton.me>
2025-07-06 15:59:37 -0400

    Moved to LTS hardened

--- config/system.nix
+++ config/system.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_14_hardened;
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_hardened;
   environment.enableAllTerminfo = true;
   system.stateVersion = "25.11";
   nixpkgs.config.allowUnfree = true;
